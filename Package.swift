3cda515e720843423c3777886d83497c7ddd23b2
hberenger <herve.berenger@mwm.io>
2023-09-04 03:12:30 +0200

    Enable WASI applications by making m3_LinkWASI available

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             cSettings: [
                 .define("APPLICATION_EXTENSION_API_ONLY", to: "YES"),
                 .define("d_m3MaxDuplicateFunctionImpl", to: "10"),
+                .define("__wasi__", to: "YES"),
+                .define("d_m3HasWASI", to: "YES"),
+                //for debugging: .define("d_m3EnableStrace", to: "2")
             ]
         ),
         .testTarget(
